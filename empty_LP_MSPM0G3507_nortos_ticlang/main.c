d1879debc49fa7538ec03feead722211ed64df8c
xuanxuan66688 <1840653099@qq.com>
2026-08-12 22:17:53 +0800

    测试OLED显示函数

--- empty_LP_MSPM0G3507_nortos_ticlang/main.c
+++ empty_LP_MSPM0G3507_nortos_ticlang/main.c
@@ -41,42 +41,15 @@ int main(void)
     SYSCFG_DL_init();
     SysTick_Init();
 
-    MPU6050_Init();
     OLED_Init();
 
     /* Don't remove this! */
     Interrupt_Init();
 
-    OLED_ShowString(0,7,(uint8_t *)"MPU6050 Demo",8);
-
-    OLED_ShowString(0,0,(uint8_t *)"Pitch",8);
-    OLED_ShowString(0,2,(uint8_t *)" Roll",8);
-    OLED_ShowString(0,4,(uint8_t *)"  Yaw",8);
-
-    OLED_ShowString(16*6,3,(uint8_t *)"Accel",8);
-    OLED_ShowString(17*6,4,(uint8_t *)"Gyro",8);
+    OLED_ShowString(1,1,"Hello World!",16);
 
     while (1) 
     {
-       // sprintf((char *)oled_buffer, "%-6.1f", pitch);
-        OLED_ShowString(5*8,0,oled_buffer,16);
-        sprintf((char *)oled_buffer, "%-6.1f", roll);
-        OLED_ShowString(5*8,2,oled_buffer,16);
-        sprintf((char *)oled_buffer, "%-6.1f", yaw);
-        OLED_ShowString(5*8,4,oled_buffer,16);
-
-        sprintf((char *)oled_buffer, "%6d", accel[0]);
-        OLED_ShowString(15*6,0,oled_buffer,8);
-        sprintf((char *)oled_buffer, "%6d", accel[1]);
-        OLED_ShowString(15*6,1,oled_buffer,8);
-        sprintf((char *)oled_buffer, "%6d", accel[2]);
-        OLED_ShowString(15*6,2,oled_buffer,8);
-
-        sprintf((char *)oled_buffer, "%6d", gyro[0]);
-        OLED_ShowString(15*6,5,oled_buffer,8);
-        sprintf((char *)oled_buffer, "%6d", gyro[1]);
-        OLED_ShowString(15*6,6,oled_buffer,8);
-        sprintf((char *)oled_buffer, "%6d", gyro[2]);
-        OLED_ShowString(15*6,7,oled_buffer,8);
+       
     }
 }
